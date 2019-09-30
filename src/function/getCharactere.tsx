a0fc4a25cb48969539d41227fd4305276807bc21
Aloisio Alessandro <alessaloisio@gmail.com>
2019-09-30 14:46:03 +0200

    interface, lists avec images

--- src/function/getCharactere.tsx
+++ src/function/getCharactere.tsx
@@ -1,28 +1,16 @@
 import axios from "axios";
 const getCharactere = async () => {
-
-
-    await axios.get('https://character-database.becode.xyz/characters')
+  await axios.get("https://character-database.becode.xyz/characters");
 
   try {
     const res = await axios.get(
       `https://character-database.becode.xyz/characters`
     );
-    const array = res.data;
 
-      array.forEach(function(element) {
-          let newarray = {};
-          newarray.push(element);
-          console.log(element);
-          console.log(newarray);
-      });
-      return res.data;
-
+    return res.data;
   } catch (e) {
     return null;
   }
-
 };
-
 
 export default getCharactere;
