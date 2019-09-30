3189138f4aa10d0d9bba20c807808244aa2bb83f
Aloisio Alessandro <alessaloisio@gmail.com>
2019-09-30 12:22:28 +0200

    lists design

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
+
+import Header from "./components/Header/Header";
+import ListCharacter from "./components/ListsCharacter/ListsCharacter";
 
 import "./App.css";
 
 const App: React.FC = () => (
   <div className="App">
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <Header />
+    <ListCharacter />
   </div>
 );
 
