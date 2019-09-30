48507578ace7db1896a2d26de48377a2c86522dd
Aloisio Alessandro <alessaloisio@gmail.com>
2019-09-30 10:47:47 +0200

    add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "@material-ui/core/Button";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+
+const App: React.FC = () => (
+  <div className="App">
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  </div>
+);
 
 export default App;
