d8efd508cee4d921ecdd4df264361efd151a0e07
Aloisio Alessandro <alessaloisio@gmail.com>
2019-10-02 11:36:31 +0200

    Updates

---
+++ precache-manifest.82cd0319be4d7ff3e01e09c40deb588f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d421e36e9a75c9113ed5c55bbfc91a",
+    "url": "/character-manager/index.html"
+  },
+  {
+    "revision": "b30e3b26a520f6b825d8",
+    "url": "/character-manager/static/css/main.a2deb82c.chunk.css"
+  },
+  {
+    "revision": "fb02e5d0ca5a5fc7684b",
+    "url": "/character-manager/static/js/2.80d04e78.chunk.js"
+  },
+  {
+    "revision": "b30e3b26a520f6b825d8",
+    "url": "/character-manager/static/js/main.4fd03785.chunk.js"
+  },
+  {
+    "revision": "0c7a40b273b886fd34ef",
+    "url": "/character-manager/static/js/runtime-main.3f531b0c.js"
+  }
+]);
